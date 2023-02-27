e85fc8d22d348b792856f1938257fdd1e945a972
AfonsoCoelho2007 <122289626+AfonsoCoelho2007@users.noreply.github.com>
2023-02-27 14:41:03 +0000

    .

--- Makefile
+++
@@ -1,8 +0,0 @@
-a.out:
-	gcc exercicio1.c
-
-clean:
-	rm exercicio1
-
-test: a.out
-	bash test.sh
--- exercicio1.c
+++ exercicio1.c
@@ -1,2 +1,24 @@
-// Escreva o seu código a seguir
+#include <stdio.h>
+
+int main() {
+    int n,n2,n3,res;
+    char palavra[80];
+    printf("O meu nome:\n");
+    gets (palavra);
+    printf("\n");
+        scanf("%d",&n);
+    printf("\n");
+        scanf("%d", &n2);
+    printf("\n");
+        scanf("%d",&n3);
+
+res=n2-n;
+
+printf("+-------------------+\n");
+printf("| Dias de vida para |\n");
+printf("| %3s   |\n",palavra);
+printf("+-------------------+\n");
+printf("| %3d          |\n",res);
+printf("+-------------------+\n");
+}
 
